31b45fa2fbe1d0579c39f41068bde031aa8f39f0
Paul Müller <pa.muel@web.de>
2020-07-02 16:33:06 +0200

    Exercise 1.5 bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+start_height = 100.0
+bounce_height = 0
+counter = 1
+
+while counter <= 10:
+	bounce_height = ((start_height /5) * 3)
+	print(counter ,' : ', bounce_height, ' meters')
+	start_height = bounce_height
+	counter = counter + 1
